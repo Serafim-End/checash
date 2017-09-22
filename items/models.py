6d56548ef5d7cdc598063cf3b0e66f4c26b1351d
Serafim End <serafim-end@yandex.ru>
2017-09-22 16:36:41 +0300

    url field for item model was added

--- items/models.py
+++ items/models.py
@@ -9,6 +9,8 @@ class Item(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=128, blank=True, default='')
 
+    url = models.URLField(null=True)
+
     price = models.IntegerField(default=-1)
     cashback = models.IntegerField(default=-1)
 
