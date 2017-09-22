a2025142f618e043f39fa1cc8c0c10162012040e
Serafim End <serafim-end@yandex.ru>
2017-09-22 22:34:10 +0300

    raven release

--- checash/settings.py
+++ checash/settings.py
@@ -190,7 +190,8 @@ RAVEN_CONFIG = {
     ),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    'release': os.environ['HEROKU_SLUG_COMMIT']
     # 'CELERY_LOGLEVEL': logging.INFO
 }
 
