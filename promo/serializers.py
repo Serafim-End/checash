57aa97cd9ea2af7f4cdf5b7540b18ddf392bb3d3
Serafim End <serafim-end@yandex.ru>
2017-09-25 16:42:38 +0300

    item serializer was added for

--- promo/serializers.py
+++ promo/serializers.py
@@ -10,7 +10,7 @@ from .models import Promo
 
 class PromoSerializer(serializers.ModelSerializer):
 
-    # items = ItemSerializer(many=True)
+    items = ItemSerializer()
 
     class Meta:
         model = Promo
