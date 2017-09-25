e9a2c532f3174a95bcbc08f5088a5730455d2d64
Serafim End <serafim-end@yandex.ru>
2017-09-25 11:45:39 +0300

    person migration

--- user/models.py
+++ user/models.py
@@ -7,7 +7,7 @@ from bill.models import Bill
 class Person(User):
 
     bills = models.ForeignKey(Bill, on_delete=models.CASCADE, null=True)
-    total_cashback = models.IntegerField()
-    current_cashback = models.IntegerField()
+    total_cashback = models.IntegerField(default=0)
+    current_cashback = models.IntegerField(default=0)
 
     creation_time = models.DateTimeField(auto_now=True)
