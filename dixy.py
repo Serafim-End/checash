709fb7f6c67c1978245e95a1a5732b5b3d149313
kate-chuikova <chuikova@phystech.edu>
2017-09-25 12:58:16 +0300

    Update dixy.py

--- dixy.py
+++ dixy.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
 import requests
 from lxml import html
+import re
+import datetime
 
 url = 'https://dixy.ru/akcii/'
 response = requests.get(url)
@@ -10,7 +11,7 @@ response = requests.get(url)
 parsed_body = html.fromstring(response.text)
 
 l = len(parsed_body.xpath('//div[@class="product"]/div/div/img/@alt'))
-
+#print(l)
 for i in range(l):
     name = parsed_body.xpath('//div[@class="product"]/div/div/img/@alt')[i]
     photo = parsed_body.xpath('//div[@class="product"]/div/div/img/@src')[i]
@@ -22,10 +23,23 @@ for i in range(l):
     title = parsed_body.xpath('//div[@class="product-day" or @class="product-title"]//text()')[i]
     promo = parsed_body.xpath('//div[@class="discount  " or @class="value"]/text()')[i]
 
+    data = re.findall(r'\d{2}.\d{2}.\d{2,4}', title)
+    if len(data) == 0:
+        promo_start = datetime.datetime.today()
+        promo_end = datetime.datetime.today() + datetime.timedelta(days=1)
+    elif len(data) == 1:
+        promo_start = datetime.datetime.today()
+        promo_end = datetime.datetime.strptime(data[0], "%d.%m.%Y")
+    else:
+        promo_start = datetime.datetime.strptime(data[0], "%d.%m.%Y")
+        promo_end = datetime.datetime.strptime(data[1], "%d.%m.%Y")
+
     print name
-    print 'https://dixy.ru' + photo
+    print url + photo
     print category
     print title
+    print promo_start
+    print promo_end
     print 'Новая цена (рубли):',  price_new
     print 'Новая цена (копейки): ',  fract_new
     print 'Старая цена (рубли):',price_old
