c5bc3c5700ac85910189ca43c857d187da399523
Serafim End <serafim-end@yandex.ru>
2017-09-26 15:17:49 +0300

    codecov was deleted, tests were added

--- promo/tests.py
+++ promo/tests.py
@@ -1,3 +1,31 @@
+
 from django.test import TestCase
 
-# Create your tests here.
+from .service import PromoService
+from .models import Promo
+
+
+class PromoServiceTestCases(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_actual_promos(self):
+
+        actual_promos = PromoService.get_actual_promos()
+
+        self.assertGreater(
+            len(actual_promos), 0,
+            msg='no actual promos'
+        )
+
+        self.assertIsInstance(
+            actual_promos, list,
+            msg='promos represents not a list'
+        )
+
+        self.assertIsInstance(
+            actual_promos.pop(), Promo,
+            msg='element of actual_promos is not Promo instance'
+        )
+
