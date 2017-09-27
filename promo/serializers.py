90a1e375e38d927966448ed09e18de1d0e3e7fe2
Serafim End <serafim-end@yandex.ru>
2017-09-27 19:11:27 +0300

    flat serializer

--- promo/serializers.py
+++ promo/serializers.py
@@ -3,14 +3,17 @@ from rest_framework import serializers
 
 from .models import Promo
 
-from items.serializers import ItemSerializer
+# from items.serializers import ItemSerializer
 
 
 class PromoSerializer(serializers.ModelSerializer):
 
-    items = ItemSerializer(many=False)
+    # items = ItemSerializer(many=False)
+    url = serializers.CharField(read_only=True, source='items.url')
+    description = serializers.CharField(read_only=True,
+                                        source='items.description')
 
     class Meta:
         model = Promo
         fields = ('title', 'due_date', 'start_date', 'cashback_rub',
-                  'items', 'lower_bound_rub')
+                  'lower_bound_rub', 'url', 'description')
