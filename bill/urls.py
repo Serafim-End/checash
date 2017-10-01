b9c09f34096c0dd4ab0ea23ee40197e35eaf74f8
Serafim End <serafim-end@yandex.ru>
2017-10-01 15:28:43 +0300

    url to get the bill via fiscalSign

--- bill/urls.py
+++ bill/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import url
 
-from .views import BillView
+from .views import BillView, BillFPView
 
 
 urlpatterns = [
     url(r'^bill/', BillView.as_view()),
+    url(r'^bill/fp/', BillFPView.as_view())
 ]
